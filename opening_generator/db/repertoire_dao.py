6cd84fe90b02901f1417fa8c7b762a14cbe83432
Emanuel <emanuel.brea@tsgforce.com>
2022-06-20 01:02:34 -0300

    Permito editar una variante sin indicar una jugada. En ese caso, va a intentar elegir una distinta a la anterior. Si no puede en 10 intentos, falla.

--- opening_generator/db/repertoire_dao.py
+++ opening_generator/db/repertoire_dao.py
@@ -1,7 +1,9 @@
 import logging
+from typing import List
 
+from opening_generator import User
 from opening_generator.db import db_session
-from opening_generator.models import Repertoire
+from opening_generator.models import Repertoire, Move
 
 
 class RepertoireDao:
@@ -9,27 +11,24 @@ class RepertoireDao:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def create_repertoire(self, user, color, moves):
+    def create_repertoire(self, user: User, color: bool, moves: List[Move]):
         self.delete_repertoire(user, color)
         repertoire = Repertoire(user=user, color=color, moves=moves)
         db_session.add(repertoire)
         db_session.commit()
         self.logger.info("Created %s repertoire for user %s.", "white" if color else "black", user.email)
 
-    def delete_repertoire(self, user, color):
-        db_session.query(Repertoire).filter(
-            Repertoire.user_id == user.user_id,
-            Repertoire.color == color
-        ).delete()
+    def delete_repertoire(self, user: User, color: bool):
+        user.repertoire = [repertoire for repertoire in user.repertoire if repertoire.color != color]
         self.logger.info("Deleted %s repertoire for user %s.", "white" if color else "black", user.email)
 
-    def delete_moves_from_repertoire(self, repertoire, moves, user, color):
+    def delete_moves_from_repertoire(self, repertoire: Repertoire, moves, user: User, color: bool):
         repertoire.moves = list(set(repertoire.moves) - set(moves))
         db_session.commit()
         self.logger.info("Deleted %d moves from %s repertoire for user %s.", len(moves), "white" if color else "black",
                          user.email)
 
-    def insert_new_moves(self, repertoire, moves, user, color):
+    def insert_new_moves(self, repertoire: Repertoire, moves: List[Move], user: User, color: bool):
         repertoire.moves += moves
         db_session.commit()
         self.logger.info("Inserted %d moves from %s repertoire for user %s.", len(moves), "white" if color else "black",
--- opening_generator/services/repertoire_service.py
+++ opening_generator/services/repertoire_service.py
@@ -68,15 +68,27 @@ class RepertoireService:
 
     def update_user_repertoire(self, position, user, color, new_move, old_move):
         next_moves = position.next_moves
-        new_move = next((move for move in next_moves if move.move_san == new_move), None)
         old_move = next((move for move in next_moves if move.move_san == old_move), None)
-        if not new_move or not old_move:
+        if not old_move:
             return []
-        repertoire = next(repertoire for repertoire in user.repertoire if repertoire.color == color)
-        repertoire_moves = repertoire.moves
+        if not new_move:
+            tries = 0
+            new_move = picker_service.pick_move(position, user, color)
+            while new_move is old_move:
+                new_move = picker_service.pick_move(position, user, color)
+                tries += 1
+                if tries == 10:
+                    return []
+        else:
+            new_move = next((move for move in next_moves if move.move_san == new_move), None)
+            if not new_move:
+                return []
         next_position = new_move.next_position
         moves = picker_service.pick_variations(next_position, user, color)
         moves.append(new_move)
+
+        repertoire = next(repertoire for repertoire in user.repertoire if repertoire.color == color)
+        repertoire_moves = repertoire.moves
         moves_to_remove = self.get_moves_after_position(repertoire_moves, old_move)
         repertoire_dao.delete_moves_from_repertoire(repertoire=repertoire, user=user, color=color,
                                                     moves=moves_to_remove)
