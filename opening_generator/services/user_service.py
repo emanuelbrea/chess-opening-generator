3002a7a518e8182b7c56b48a8945f7088be7990d
emanuelbrea <ebrea@uade.edu.ar>
2022-07-03 21:01:23 -0300

    Agrego endpoint para crear style

--- opening_generator/services/user_service.py
+++ opening_generator/services/user_service.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import IntegrityError
 
 from opening_generator.db.user_dao import user_dao
 from opening_generator.exceptions import UserException
+from opening_generator.models import Style, User
 
 
 class UserService:
@@ -16,6 +17,13 @@ class UserService:
             user_dao.create_user(first_name=first_name, email=email)
         except IntegrityError as err:
             raise UserException(f"User with email {email} already exists.") from err
+
+    def create_user_style(self, user: User, popularity: float, fashion: float, risk: float, rating: int):
+        style = Style(popularity=popularity, fashion=fashion, risk=risk, rating=rating)
+        try:
+            user_dao.add_style_to_user(user=user, style=style)
+        except IntegrityError as err:
+            raise UserException(f"Invalid style.") from err
 
 
 user_service = UserService()
