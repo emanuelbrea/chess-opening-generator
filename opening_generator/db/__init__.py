099fbacf8b5ecde01089ff6b6426ae42d85b3fb5
emanuelbrea <ebrea@uade.edu.ar>
2022-07-24 23:23:47 -0300

    Integro con aws elastic beanstalk Compatible con python 3.8

--- opening_generator/db/__init__.py
+++ opening_generator/db/__init__.py
@@ -4,12 +4,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(environ.get('DATABASE_URL'))
+if 'RDS_HOSTNAME' in environ:
+    engine = create_engine(
+        f'postgresql://{environ["RDS_USERNAME"]}:{environ["RDS_PASSWORD"]}@{environ["RDS_HOSTNAME"]}:{environ["RDS_PORT"]}/{environ["RDS_DB_NAME"]}')
+else:
+    engine = create_engine(environ.get('DATABASE_URL'))
+
 db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
 def init_db():
-    import opening_generator.models
     Base.metadata.create_all(bind=engine)
