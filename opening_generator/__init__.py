a5122c56314e2a4a6005f0a8110d336cce4f00e1
emanuelbrea <ebrea@uade.edu.ar>
2022-10-02 15:58:15 -0300

    Improved algorithm

--- opening_generator/__init__.py
+++ opening_generator/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-        app.config.from_object("config.DevConfig")
+        app.config.from_object("config.ProdConfig")
     else:
         app.config.from_mapping(test_config)
 
