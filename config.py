5e30657a81aad9b1e55438b00164d64cf8e7d2e9
emanuelbrea <ebrea@uade.edu.ar>
2022-10-22 15:06:59 -0300

    Upgraded flask version

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-from os import environ, path
+from os import path
 from dotenv import load_dotenv
+import secrets
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
@@ -7,7 +8,7 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = secrets.token_hex()
     AWS_REGION = 'us-east-1'
     COGNITO_CLIENT_ID = '1rnu18aka8gk8k9hrjlttg40cs'
     COGNITO_POOL_ID = 'us-east-1_GFAp0SDxh'
